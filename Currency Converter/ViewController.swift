b9339a5b1fe9c6a693152ec5aaf2d6cd66a8dcf3
Handy <handy@Athena.local>
2015-09-07 16:02:28 -0300

    Initial Commit

---
+++ Currency Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Handy on 07/09/15.
+//  Copyright (c) 2015 RU Informática. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Currency ConverterTests/Currency_ConverterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Currency_ConverterTests.swift
+//  Currency ConverterTests
+//
+//  Created by Handy on 07/09/15.
+//  Copyright (c) 2015 RU Informática. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Currency_ConverterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
